fadbf09b0808df5fd6afec9e5343b93ba564bbd3
Roozbeh <roozbehsayadi@gmail.com>
2020-05-28 13:45:55 +0430

    Implemented text inputs for form.

--- front/src/components/MyForm.js
+++ front/src/components/MyForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 
-import { Typography, Form, Input, Button } from 'antd'
+import { Typography, Form, Input, Select } from 'antd'
 import { Layout } from 'antd'
 
 const axios = require('axios').default
@@ -13,9 +13,11 @@ class MyForm extends React.Component {
 			fields: [],
 			title: '',
 			id: 0,
+			componentCounter: 0,
 		}
 
 		this.handleFormCreation = this.handleFormCreation.bind(this)
+		this.getRespectiveComponent = this.getRespectiveComponent.bind(this)
 		// this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
@@ -28,7 +30,7 @@ class MyForm extends React.Component {
 	}
 
 	componentDidMount() {
-		console.log('component did mount')
+		// console.log('component did mount')
 		const id = this.props.match.params.id
 		axios({
 			method: 'get',
@@ -42,25 +44,74 @@ class MyForm extends React.Component {
 			})
 	}
 
-	// handleSubmit() {}
+	handleSubmit() {}
+
+	getTextComponent(description, index) {
+		const { Option } = Select
+
+		return (
+			<Form.Item
+				name={description.name}
+				label={description.title}
+				key={index}
+				rules={[
+					{
+						required: description.hasOwnProperty('required')
+							? description.required
+							: false,
+					},
+				]}
+				style={{ display: 'inline-block' }}
+			>
+				{description.hasOwnProperty('options') ? (
+					<Select allowClear>
+						{description.options.map((option, optionIndex) => {
+							return (
+								<Option value={option.value} key={optionIndex}>
+									{option.label}
+								</Option>
+							)
+						})}
+					</Select>
+				) : (
+					<Input />
+				)}
+			</Form.Item>
+		)
+	}
+
+	getRespectiveComponent(description, index) {
+		if (description.type === 'Text')
+			return this.getTextComponent(description, index)
+		else return <h1 key={index}>Temp</h1>
+	}
 
 	render() {
 		const { Title } = Typography
 		const { Header, Footer, Sider, Content } = Layout
 
+		const headerTextStyle = { color: '#333333', marginLeft: '0' }
+
+		const formItems = this.state.fields.map((field, index) => {
+			return this.getRespectiveComponent(field, index)
+		})
+
 		return (
 			<>
 				<Layout>
-					<Header style={{ textAlign: 'center' }}>
-						<Title
-							style={
-								({ color: '#111111' },
-								{ display: 'inline-block' })
-							}
-						>
+					<Header>
+						<Title style={headerTextStyle}>
 							{this.state.title}
 						</Title>
 					</Header>
+					<Content style={{ textAlign: 'left', marginLeft: '7%' }}>
+						<Form
+							name={'form_' + this.state.id}
+							onFinish={this.handleSubmit}
+						>
+							{formItems}
+						</Form>
+					</Content>
 				</Layout>
 			</>
 		)
