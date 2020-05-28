a3feec4ca010fec992ffe1b85bb782088dc77c49
Roozbeh <roozbehsayadi@gmail.com>
2020-05-28 20:42:12 +0430

    Added environment variables to back-end.

--- back/src/app.js
+++ back/src/app.js
@@ -3,10 +3,13 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const router = require('./controller')
+require('dotenv').config()
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.json())
 app.use('/api', router)
 
-app.listen(8000, () => console.log(`Server listening on port ${8000}!`))
+app.listen(process.env.PORT, () =>
+	console.log(`Server listening on port ${process.env.PORT}!`)
+)
