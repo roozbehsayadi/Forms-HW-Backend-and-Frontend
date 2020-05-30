c336ee0588d24682283b991a52d123af672f0aea
Roozbeh <roozbehsayadi@gmail.com>
2020-05-30 23:17:28 +0430

    Added persian language and rtl direction to main page. You can choose your language by a switch at the end of the page.

--- front/src/components/Forms.js
+++ front/src/components/Forms.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { withRouter } from 'react-router-dom'
 
-import { Typography, Table } from 'antd'
+import { Typography, Table, Switch } from 'antd'
 
 import { Layout } from 'antd'
+
+import { ConfigProvider } from 'antd'
 
 const axios = require('axios')
 
@@ -12,11 +14,14 @@ class Forms extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			title: "Forms' List",
+			title_en: "Forms' List",
+			title_fa: 'لیست فرم‌ها',
 			forms: [],
+			language: 'EN',
 		}
 
 		this.handleTableCreation = this.handleTableCreation.bind(this)
+		this.handleLanguageSwitch = this.handleLanguageSwitch.bind(this)
 	}
 
 	handleTableCreation = (data) => {
@@ -42,9 +47,15 @@ class Forms extends React.Component {
 		this.props.history.push(path)
 	}
 
+	handleLanguageSwitch() {
+		this.setState((prevState) => {
+			return { language: prevState.language === 'EN' ? 'FA' : 'EN' }
+		})
+	}
+
 	render() {
 		const { Title } = Typography
-		const { Header, Content } = Layout
+		const { Header, Content, Footer } = Layout
 
 		const headerTextStyle = {
 			color: '#333333',
@@ -52,7 +63,7 @@ class Forms extends React.Component {
 			display: 'inline-block',
 			height: '100%',
 			position: 'absolute',
-			top: '0',
+			top: '10px',
 		}
 
 		const forms = this.state.forms.map((form, index) => {
@@ -63,61 +74,100 @@ class Forms extends React.Component {
 			}
 		})
 
-		const columns = [
-			{
-				title: '#',
-				dataIndex: 'key',
-				key: 'key',
-				width: '1%',
-			},
-			{
-				title: 'id',
-				dataIndex: 'id',
-				key: 'id',
-				width: '4%',
-			},
-			{
-				title: 'title',
-				dataIndex: 'title',
-				key: 'title',
-			},
-		]
+		const columns =
+			this.state.language === 'EN'
+				? [
+						{
+							title: '#',
+							dataIndex: 'key',
+							key: 'key',
+							width: '1%',
+						},
+						{
+							title: 'id',
+							dataIndex: 'id',
+							key: 'id',
+							width: '4%',
+						},
+						{
+							title: 'title',
+							dataIndex: 'title',
+							key: 'title',
+						},
+				  ]
+				: [
+						{
+							title: '#',
+							dataIndex: 'key',
+							key: 'key',
+							width: '1%',
+						},
+						{
+							title: 'شناسه',
+							dataIndex: 'id',
+							key: 'id',
+							width: '4%',
+						},
+						{
+							title: 'عنوان',
+							dataIndex: 'title',
+							key: 'title',
+						},
+				  ]
 
 		return (
 			<>
-				<Layout>
-					<Header
-						style={{
-							overflow: 'hidden',
-							position: 'relative',
-							width: '100%',
-						}}
-					>
-						<Title style={headerTextStyle}>
-							{this.state.title}
-						</Title>
-					</Header>
-					<Content
-						style={{
-							textAlign: 'left',
-							marginLeft: '7%',
-							width: '86%',
-						}}
-					>
-						<Table
-							dataSource={forms}
-							columns={columns}
-							onRow={(record, index) => ({
-								onClick: () => {
-									console.log('clicked on id ' + record.id)
-									this.nextPath(
-										'/submit_form/' + record.id + '/'
-									)
-								},
-							})}
-						/>
-					</Content>
-				</Layout>
+				<ConfigProvider
+					direction={this.state.language === 'EN' ? 'ltr' : 'rtl'}
+				>
+					<Layout>
+						<Header
+							style={{
+								overflow: 'hidden',
+								position: 'relative',
+								width: '100%',
+							}}
+						>
+							<Title style={headerTextStyle}>
+								{this.state.language === 'EN'
+									? this.state.title_en
+									: this.state.title_fa}
+							</Title>
+						</Header>
+
+						<Content
+							style={{
+								textAlign: 'left',
+								marginLeft: '7%',
+								width: '86%',
+								marginTop: '2%',
+							}}
+						>
+							<Table
+								dataSource={forms}
+								columns={columns}
+								onRow={(record, index) => ({
+									onClick: () => {
+										console.log(
+											'clicked on id ' + record.id
+										)
+										this.nextPath(
+											'/submit_form/' + record.id + '/'
+										)
+									},
+								})}
+							/>
+						</Content>
+						<Footer>
+							<Switch
+								checkedChildren="فارسی"
+								unCheckedChildren="English"
+								onChange={this.handleLanguageSwitch}
+								size="default"
+							/>
+						</Footer>
+					</Layout>
+				</ConfigProvider>
 			</>
 		)
 	}
