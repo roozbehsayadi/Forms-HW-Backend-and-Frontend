f1e8a2c44c5701d235270c843e76a83b7379b45d
amirali <amirali.mirian@gmail.com>
2020-06-03 16:46:59 +0430

    added path package

--- back/src/app.js
+++ back/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const router = require('./controller')
 require('dotenv').config()
 
