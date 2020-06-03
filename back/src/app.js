a9c4c099c911e293719e3a58ce7e26aee780e4ae
amirali <amirali.mirian@gmail.com>
2020-06-03 15:21:38 +0430

    made some changes for heroku deployment. dont know if it works XD

--- back/src/app.js
+++ back/src/app.js
@@ -10,6 +10,15 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use('/api', router)
 
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+}
+
 app.listen(process.env.PORT, () =>
 	console.log(`Server listening on port ${process.env.PORT}!`)
 )
